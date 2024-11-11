1c6dcdbbae0c55fc3b7152d9e1be30d1e0fd4989
nidhinagpure <nagpurenidhi8@gmail.com>
2024-11-11 23:43:44 +0530

    function name change

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function openbox(){
+    const overElement = document.getElementById("overlay");
+    overElement.style.display='flex';
+}
+
+function closebox(){
+    const overElement = document.getElementById("overlay");
+    overElement.style.display='none'
+}
+  function placeOrder(){
+    alert(' Order successfull 😋');
+  }
+  
+
+
